654e44b1482893fc6c3f4649c4eaa3e09172a037
suveergarg <gargsuveer@gmail.com>
2022-01-26 14:17:41 -0500

    added colors to branch parse

--- setup_bashrc.sh
+++ setup_bashrc.sh
@@ -18,8 +18,15 @@ vol_down(){
  amixer -D pulse sset Master 10%-
 }
 
+force_color_prompt=yes
+color_prompt=yes
 parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+ git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
-export PS1='\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ '
+if [ "$color_prompt" = yes ]; then
+ PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
+else
+ PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\$ '
+fi
+unset color_prompt force_color_prompt
 "
